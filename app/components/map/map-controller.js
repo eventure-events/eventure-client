ad617265dd28b3b5e5cc36dad876d5b1de636d86
Shane Henning <shanerhenning@gmail.com>
2016-09-13 17:35:45 -0700

    markers populate for events, links on click

--- app/components/map/map-controller.js
+++ app/components/map/map-controller.js
@@ -7,52 +7,30 @@ module.exports = exports = (app) => {
 function MapController($log, dataService, eventService) {
   eventService.allEvents()
     .then(() => {
-
-      $log.log('MapController dataService.events: ', dataService.events);
-
-
-
       const mapEle = document.getElementById('map');
       const center = {
-        lat: 44,
-        lng: -120,
+        lat: 47.6205379,
+        lng: -122.3491348,
       };
-
       const mapOptions = {
-        zoom: 5,
+        zoom: 14,
         center: center,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
       };
-
       let map = this.map = new google.maps.Map(mapEle, mapOptions);
 
-      var geocoder = new google.maps.Geocoder();
-      var address = '2901 3rd Ave, Seattle, WA';
 
-      geocoder.geocode({
-        'address': address
-      }, function(results, status) {
-
-        if (status == google.maps.GeocoderStatus.OK) {
-          const pin = {
-            lat: results[0].geometry.location.lat(),
-            lng: results[0].geometry.location.lng()
-          };
-          var marker = new google.maps.Marker({
-            position: pin,
-            map: map,
-          });
-
-          let infoWindow = new google.maps.InfoWindow();
-          infoWindow.setContent(address);
-
-          marker.addListener('click', function() {
-            infoWindow.open(map, marker);
-          });
-
-        }
+      dataService.events.forEach(function(item) {
+        let marker = new google.maps.Marker({
+          position: item.latLong,
+          map: map,
+        });
+        let infoWindow = new google.maps.InfoWindow({
+          content: '<a href="#/event/' + item._id + '">' + item.name + '</a><br/>' + item.location + '<br/>' + item.description,
+        });
+        marker.addListener('click', function() {
+          infoWindow.open(map, marker);
+        });
       });
-
-
     });
 }
