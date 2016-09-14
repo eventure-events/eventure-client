f22c652ca7db3bdcc18751c85ca6f4db6621e7fc
Jeffrey Gebhardt <jeff@gebbey.com>
2016-09-14 15:26:49 -0700

    list view on home page

--- app/components/display/display-directive.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  app.component('evDisplayComponent', {
-    controller: 'DisplayController',
-    template: require('./display-template.html'),
-  });
-};
