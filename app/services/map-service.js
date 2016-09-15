428b3812827eb4e63895627c6372ca8f8613cc35
Shane Henning <shanerhenning@gmail.com>
2016-09-15 12:23:45 -0700

    you are here marker

--- app/services/map-service.js
+++ app/services/map-service.js
@@ -10,7 +10,6 @@ function mapService($rootScope, dataService) {
   service.map;
   service.labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   service.labelIndex = 0;
-  service.you = require('../resources/you.svg');
   dataService.viewportEvents = [];
 
 
@@ -23,97 +22,6 @@ function mapService($rootScope, dataService) {
     };
     const mapOptions = {
       zoom: 14,
-      styles: [{
-        elementType: 'geometry',
-        stylers: [{
-          hue: '#ff4400',
-        }, {
-          saturation: -68,
-        }, {
-          lightness: -4,
-        }, {
-          gamma: 0.72,
-        }],
-      }, {
-        featureType: 'road',
-        elementType: 'labels.icon',
-      }, {
-        featureType: 'landscape.man_made',
-        elementType: 'geometry',
-        stylers: [{
-          hue: '#0077ff',
-        }, {
-          gamma: 3.1,
-        }],
-      }, {
-        featureType: 'water',
-        stylers: [{
-          hue: '#0b2236',
-        }, {
-          gamma: .25,
-        }, {
-          saturation: -20,
-        }],
-      }, {
-        featureType: 'poi.park',
-        stylers: [{
-          hue: '#44ff00',
-        }, {
-          saturation: -23,
-        }],
-      }, {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          hue: '#007fff',
-        }, {
-          gamma: 0.77,
-        }, {
-          saturation: 65,
-        }, {
-          lightness: 99,
-        }],
-      }, {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{
-          gamma: 0.11,
-        }, {
-          weight: 5.6,
-        }, {
-          saturation: 99,
-        }, {
-          hue: '#0091ff',
-        }, {
-          lightness: -86,
-        }],
-      }, {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{
-          lightness: -48,
-        }, {
-          hue: '#ff5e00',
-        }, {
-          gamma: 1.2,
-        }, {
-          saturation: -23,
-        }],
-      }, {
-        featureType: 'transit',
-        elementType: 'labels.text.stroke',
-        stylers: [{
-          saturation: -64,
-        }, {
-          hue: '#ff9100',
-        }, {
-          lightness: 16,
-        }, {
-          gamma: 0.47,
-        }, {
-          weight: 2.7,
-        }],
-      }],
       center: center,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
